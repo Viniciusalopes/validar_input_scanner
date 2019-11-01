530a1e7e97827a9a70749a54a509873fdc8a59f3
viniciusalopes <suporte@viniciusalopes.com.br>
2019-11-01 14:06:52 -0300

    Manupulação de Strings

---
+++ ValidarInputScanner/src/util/Textos.java
@@ -0,0 +1,52 @@
+/*
+ * A licença MIT
+ *
+ * Copyright 2019 Viniciusalopes Tecnologia <suporte@viniciusalopes.com.br>.
+ *
+ * É concedida permissão, gratuitamente, a qualquer pessoa que obtenha uma cópia
+ * deste software e dos arquivos de documentação associados (o "Software"), para
+ * negociar o Software sem restrições, incluindo, sem limitação, os direitos de uso,
+ * cópia, modificação e fusão, publicar, distribuir, sublicenciar e/ou vender cópias
+ * do Software, e permitir que as pessoas a quem o Software é fornecido o façam,
+ * sujeitas às seguintes condições:
+ *
+ * O aviso de copyright acima e este aviso de permissão devem ser incluídos em
+ * todas as cópias ou partes substanciais do Software.
+ *
+ * O SOFTWARE É FORNECIDO "NO ESTADO EM QUE SE ENCONTRA", SEM NENHUM TIPO DE GARANTIA,
+ * EXPRESSA OU IMPLÍCITA, INCLUINDO, MAS NÃO SE LIMITANDO ÀS GARANTIAS DE COMERCIALIZAÇÃO,
+ * ADEQUAÇÃO A UM FIM ESPECÍFICO E NÃO VIOLAÇÃO. EM NENHUMA CIRCUNSTÂNCIA, OS AUTORES
+ * OU PROPRIETÁRIOS DE DIREITOS DE AUTOR PODERÃO SER RESPONSABILIZADOS POR QUAISQUER
+ * REIVINDICAÇÕES, DANOS OU OUTRAS RESPONSABILIDADES, QUER EM AÇÃO DE CONTRATO,
+ * DELITO OU DE OUTRA FORMA, DECORRENTES DE, OU EM CONEXÃO COM O SOFTWARE OU O USO
+ * OU OUTRAS NEGOCIAÇÕES NO PROGRAMAS.
+ * ------------------------------------------------------------------------------------------
+ * Projeto   : ValidaInput
+ * Criado em : 01/11/2019
+ * Autor     : Viniciusalopes (Vovolinux) <suporte@viniciusalopes.com.br>
+ * Finalidade: Manipulação de strings
+ * ------------------------------------------------------------------------------------------
+ */
+package util;
+
+/**
+ *
+ * @author vovox
+ */
+public class Textos {
+
+    /**
+     * Repete um texto n vezes.
+     *
+     * @param texto Texto a ser repetido
+     * @param quantidade Quantidade de repetições do texto
+     * @return texto * n
+     */
+    public static String repete(String texto, int quantidade) {
+        String ret = "";
+        for (int i = 0; i < quantidade; i++) {
+            ret += texto;
+        }
+        return ret;
+    }
+}
